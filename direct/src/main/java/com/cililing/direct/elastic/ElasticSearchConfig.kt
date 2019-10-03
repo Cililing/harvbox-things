92e8e6222e55c5daca7bcf2a41d386e0a4cb62a7
Przemyslaw Materna <pmaterna@opera.com>
2019-10-03 18:39:35 +0200

    Fix package name.

--- direct/src/main/java/com/cililing/direct/elastic/ElasticSearchConfig.kt
+++ direct/src/main/java/com/cililing/direct/elastic/ElasticSearchConfig.kt
@@ -1,13 +1,12 @@
 package com.cililing.direct.elastic
 
-import com.cililing.direct.BuildConfig
 
 object ElasticSearchConfig {
     const val isEnabled = false
     const val url: String = "https://scalr.api.appbase.io"
     const val appName: String = "harvbox"
 
-    private const val credentials: String = BuildConfig.ELASTIC_SEARCH_ADMIN_KEY
+    private const val credentials: String = com.cililing.harvbox.thingsapp.common.BuildConfig.ELASTIC_SEARCH_ADMIN_KEY
     val username = credentials.split(":")[0]
     val password = credentials.split(":")[1]
 
